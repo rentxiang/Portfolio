ae5abefd950995563100cb759af9b9bf414a4c7c
Tianxiang Ren <ren.tianx@northeastern.edu>
2023-06-08 02:21:58 -0700

    update v1.0

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -94,8 +94,9 @@ function Home() {
 
           <div>
           <p className='intro' style={{ textAlign: 'left' }}>👋 Hi, there! I am an aspiring software developer who enjoys discovering new <br />ideas and technologies while diving.  </p>
-          <p className='intro' style={{ textAlign: 'left' }}>Currently I am pursuing my Masters in Information Systems at Northeastern University, interning at <Link underline="hover" key="1" color="inherit" href="https://usetract.com"><span className='bold'>TRACT</span></Link> as a software development intern.</p>
-          <p className='intro' style={{ textAlign: 'left' }}>I am a full-stack developer specializing in MERN stack Front-end and Back-end jobs. I am looking for a intern position this year or new grad job in 2024. Please feel free to get in touch with me on LinkedIn or via email.</p>
+          <p className='intro' style={{ textAlign: 'left' }}>Currently I am pursuing my Masters in Information Systems at Northeastern University, interning at <Link underline="" key="1" color="inherit" href="https://usetract.com"><span className='bold'>TRACT</span></Link> as a software development intern.</p>
+          <p className='intro' style={{ textAlign: 'left' }}>I am a full-stack developer specializing in MERN stack Front-end and Back-end jobs. I am looking for a intern position this year or new grad job in 2024. Please feel free to get in touch with me on  
+          <Link underline="" key="1" color="inherit" href="https://www.linkedin.com/in/rentxiang/"> LinkedIn</Link> or via <Link underline="" key="1" color="inherit" href="mailto:rentxiang@gmail.com">email</Link>.</p>
           </div>
           {/* <div>
             {loadingTexts.map((text, index) => (
